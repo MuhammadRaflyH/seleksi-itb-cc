9c4be1c759bc5c537da2b8f7c3508853fbbaaf71
MuhammadRaflyH <cestrafly@gmail.com>
2025-05-07 17:59:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,18 @@ import pandas as pd
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
-# load dataset
+# title
+st.title("Tugas Seleksi ITB CC - Titanic Dataset")
+
+# dataset and metadata
 path = kagglehub.dataset_download("brendan45774/test-file")
 df = pd.read_csv(f"{path}/tested.csv")
 
-df
-df.shape
+col1, col2 = st.columns(3)
+
+with col1:
+  df
+with col2:
+  st.header("A dog")
+  st.write("👋🛳️ Ahoy, welcome to Kaggle! You’re in the right place. This is the legendary Titanic ML competition – the best, first challenge for you to dive into ML competitions and familiarize yourself with how the Kaggle platform works. The competition is simple: use machine learning to create a model that predicts which passengers survived the Titanic shipwreck.")
+  df.shape
