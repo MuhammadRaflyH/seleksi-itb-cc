8254866b9c90d7b6d9b9aff5920f0bf68dce30b3
MuhammadRaflyH <cestrafly@gmail.com>
2025-05-07 16:58:07 +0700

    Update streamlit_app.py Iris Dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,38 @@
 import streamlit as st
+import kagglehub
+import pandas as pd
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
-st.title('🎈 App Name')
+# load dataset
+path = kagglehub.dataset_download("uciml/iris")
+df = pd.read_csv(f"{path}/Iris.csv")
 
-st.write('Hello world!')
+# drop ID column
+if 'Id' in df.columns:
+    df = df.drop(columns=['Id'])
+
+# extract features and labels
+features = df.drop(columns=['Species'])
+labels = df['Species']
+
+# apply PCA
+pca = PCA(n_components=2)
+components = pca.fit_transform(features)
+
+# add components to a DataFrame
+pca_df = pd.DataFrame(components, columns=['PC1', 'PC2'])
+pca_df['Species'] = labels
+
+# output dataframe
+st.title("Iris Dataset")
+st.dataframe(df)
+
+# plot PCA result
+st.subheader("2D PCA Scatter Plot")
+fig, ax = plt.subplots()
+for species in pca_df['Species'].unique():
+  subset = pca_df[pca_df['Species'] == species]
+  ax.scatter(subset['PC1'], subset['PC2'], label=species)
+ax.legend()
+st.pyplot(fig)
